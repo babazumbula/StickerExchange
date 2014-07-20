8955dd5bfe96b33f887a134c93d3b527d4d9bad2
Vukan Boskovic <vukan011@gmail.com>
2014-07-20 22:36:39 +0200

    20.07.2014.

--- src/com/example/stickerexchange/LoginActivity.java
+++ src/com/example/stickerexchange/LoginActivity.java
@@ -1,16 +1,34 @@
 package com.example.stickerexchange;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+
+
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
 public class LoginActivity extends Activity {
+	
+	EditText inputUsername;
+	EditText inputPassword;
+	Button   btnLogin;
+	ProgressBar pb;
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,10 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login);
         
         TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        inputUsername = (EditText) findViewById(R.id.usrname);
+        inputPassword = (EditText) findViewById(R.id.pass);
+        pb = (ProgressBar) findViewById(R.id.progressBar);
         
         // Listening to register new account link
         registerScreen.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +50,53 @@ public class LoginActivity extends Activity {
                 startActivity(i);
             }
         });
+        
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				
+				
+				Log.d("Button", "Login");
+				new HttpBackgorundRequest().execute();
+				
+		/*		
+				try {
+					if (json.getString(KEY_SUCCESS) != null) {
+						loginErrorMsg.setText("");
+						String res = json.getString(KEY_SUCCESS); 
+						if(Integer.parseInt(res) == 1){
+							// user successfully logged in
+							// Store user details in SQLite Database
+							DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+							JSONObject json_user = json.getJSONObject("user");
+							
+							// Clear all previous data in database
+							userFunction.logoutUser(getApplicationContext());
+							db.addUser(json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), json.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));						
+							
+							// Launch Dashboard Screen
+							Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+							
+							// Close all views before launching Dashboard
+							dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+							startActivity(dashboard);
+							
+							// Close Login Screen
+							finish();
+						}else{
+							// Error in login
+							loginErrorMsg.setText("Incorrect username/password");
+						}
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+*/
+			}
+		});
     }
 
 
@@ -49,4 +118,43 @@ public class LoginActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    class HttpBackgorundRequest extends AsyncTask<String, String, String>{
+
+    	String username = inputUsername.getText().toString();
+		String password = inputPassword.getText().toString();
+		
+		@Override
+		protected void onPreExecute() {
+		// TODO Auto-generated method stub
+		super.onPreExecute();
+
+		btnLogin.setVisibility(View.INVISIBLE);
+		pb.setVisibility(View.VISIBLE);
+		}
+		
+		@Override
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			
+			UserFunctions userFunction = new UserFunctions();
+			JSONObject json = userFunction.loginUser(username, password);
+			return null;
+		}
+    	
+		@Override
+		protected void onPostExecute(String result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+		
+		
+		pb.setVisibility(View.INVISIBLE);
+		btnLogin.setVisibility(View.VISIBLE);
+		
+
+		}
+    }
+    
+    
+    
 }
